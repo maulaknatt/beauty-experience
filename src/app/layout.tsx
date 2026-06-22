34f8e10ed624ce6df2dadcfa262c010751c8a5b6
Maulana Bagus Sudrajat <maulana712bagus@gmail.com>
2026-06-23 00:33:37 +0700

    feat: Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -130,7 +131,10 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
